6313b6f0354ef3ec34f581fb6dca0c7d1350b458
Spencer Douglas Matthews <spencer.matthews.397@cranfield.ac.uk>
2026-02-12 09:35:31 +0000

    Version 2 of post processing airfoil AoA
    Stable and full fuctionality version of the post processing script for the angle of attack sweep analysis. Currently designed to work with exported accademic paper data, but may be easily modified for experimental data from other sources. Notes are at the top of the file.

---
+++ SM_CdCl_HPC_Ver2_20251125.m
@@ -0,0 +1,327 @@
+%% AoA Sweep Analysis for CFD Data
+% This file will automatically access all angle of attack (AoA) directories
+% from the file of your choosing. You will need to edit the file header and
+% AoA values in "Define directories and AoA values" section. Properties are
+% aribrary for Re=1000, but may be modified to your liking. The loop will
+% process CFD data and calculate various performance metrics. These will be
+% printed for every AoA and graphs will be generated. A comparison of raw
+% CFD data versus the calculated values will be conducted. This information 
+% will then be automatically saved to the file of your choosing. Besure to 
+% name and date the file! Cheers!!
+%
+%Version 2: This version imports and compares data from the Ilio (2018) LBM
+%paper. This version also generates the signal trace for the lift
+%coefficicent, if you have this data. You may need to modify certain
+%aspects of the post processing script to manage your data and maintain
+%quality. Instructions are placed a sensitive locations to help you modify
+%the inputs.
+
+clc;
+clear;
+close all;
+
+%% Properties of Air (standard) and CFD inputs
+u = 1;          %[m/s] free stream velocity
+lc = 1;         %[m] Chord length
+s = 1;          %[m] span length
+rho = 1.225;    %[kg/m³] density of air
+nu = 0.001;     %Arbitrary Nu for analysis purposes
+A = s*lc;       %[m^2] wing area
+Re = (u*lc)/nu;
+
+%% Define directories and AoA values
+% Option 1: Specify directories manually
+baseDir = './NonPorous_NACA0012';  % Update this path
+aoaDirs = {'AoA0','AoA1', 'AoA2','AoA3', 'AoA4','AoA5', 'AoA6','AoA7',...
+    'AoA8','AoA9', 'AoA10','AoA11','AoA12','AoA13','AoA14','AoA15','AoA16',...
+    'AoA17','AoA18','AoA19','AoA20','AoA21','AoA22','AoA23','AoA24','AoA25'...
+    'AoA26','AoA27','AoA28'};
+aoaValues = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,...
+    24,25,26,27,28];  % Corresponding AoA values in degrees
+
+% Option 2: Automatically find directories (if they follow a pattern)
+% aoaDirs = dir(fullfile(baseDir, 'AoA_*'));
+% aoaDirs = {aoaDirs([aoaDirs.isdir]).name};
+
+%% Import Ilio (2018) paper data
+% Replace with alternative paper data, if you are using another paper
+Il_cd = readmatrix('./ilio_2018_paper_data/Ilio_CD_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+Il_cl = readmatrix('./ilio_2018_paper_data/Ilio_CL_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+Il_clcd = readmatrix('./ilio_2018_paper_data/Ilio_CLCD_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+Ku_cd = readmatrix('./ilio_2018_paper_data/Kurtulus_CD_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+Ku_cl = readmatrix('./ilio_2018_paper_data/Kurtulus_CL_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+Ku_clcd = readmatrix('./ilio_2018_paper_data/Kurtulus_CLCD_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+Kh_cl = readmatrix('./ilio_2018_paper_data/Khalid_CL_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+Li_cl = readmatrix('./ilio_2018_paper_data/Liu_CL_data.csv', 'FileType', 'text', 'NumHeaderLines', 1);
+
+%% Import signal trace data
+% Coefficient of lift signal trace data for individual runs. Replace as
+% needed with your own data.
+aoa4 = readmatrix('./NonPorous_NACA0012/AoA4/lift_coefficient-rfile.out', 'FileType', 'text', 'NumHeaderLines', 3);
+aoa10 = readmatrix('./NonPorous_NACA0012/AoA10/lift_coefficient-rfile.out', 'FileType', 'text', 'NumHeaderLines', 3);
+aoa16 = readmatrix('./NonPorous_NACA0012/AoA16/lift_coefficient-rfile.out', 'FileType', 'text', 'NumHeaderLines', 3);
+aoa28 = readmatrix('./NonPorous_NACA0012/AoA28/lift_coefficient-rfile.out', 'FileType', 'text', 'NumHeaderLines', 3);
+
+%% Plot replication from paper
+% Replicates the critical plots from the Ilio (2018) paper to be used for
+% later comparison with collected CFD and LBM data
+figure('Position', [200 200 1200 900])
+
+% Cl vs AoA
+subplot(2,2,1)
+plot(Il_cl(:,1), Il_cl(:,2), 'k-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Kh_cl(:,1),Kh_cl(:,2) , 'm-*', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Li_cl(:,1),Li_cl(:,2) , 'b-^', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Ku_cl(:,1),Ku_cl(:,2) , 'r-s', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+xlabel('Angle of Attack (degrees)')
+ylabel('Lift Coefficient (C_l)')
+title('Lift Coefficient vs AoA, Ilio(2018)')
+legend('Ilio', 'Khalid', 'Liu', 'Kurtulus', 'Location', 'best')
+grid on
+
+% Cd vs AoA
+subplot(2,2,3)
+plot(Il_cd(:,1), Il_cd(:,2), 'k-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Ku_cd(:,1),Ku_cd(:,2) , 'r-s', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+xlabel('Angle of Attack (degrees)')
+ylabel('Drag Coefficient (C_d)')
+title('Drag Coefficient vs AoA, Ilio(2018)')
+legend('Ilio', 'Kurtulus', 'Location', 'best')
+grid on
+
+% Cl/Cd vs AoA
+subplot(2,2,4)
+plot(Il_clcd(:,1), Il_clcd(:,2), 'k-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Ku_clcd(:,1),Ku_clcd(:,2) , 'r-s', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+xlabel('Angle of Attack (degrees)')
+ylabel('Aerodynamic Efficiency (C_l/C_d)')
+title('C_l/C_d vs AoA, Ilio(2018)')
+legend('Ilio', 'Kurtulus', 'Location', 'best')
+grid on
+
+%% Initialize storage arrays
+numCases = length(aoaValues);
+Cl_calc = zeros(numCases, 1);
+Cd_calc = zeros(numCases, 1);
+ClCd_calc = zeros(numCases, 1);
+Cl_solver = zeros(numCases, 1);
+Cd_solver = zeros(numCases, 1);
+ClCd_solver = zeros(numCases, 1);
+Cf_solver = zeros(numCases, 1);
+
+%% Loop through each AoA case to generate time averaged CFD data
+numValues = 10000; % Number of values to time-average
+
+for i = 1:numCases
+    currentDir = fullfile(baseDir, aoaDirs{i});
+    fprintf('\nProcessing AoA = %.1f degrees (%s)\n', aoaValues(i), aoaDirs{i});
+    
+    try
+        % Load data files
+        dragf = readmatrix(fullfile(currentDir, 'drag-rfile.out'), ...
+            'FileType', 'text', 'NumHeaderLines', 3);
+        liftf = readmatrix(fullfile(currentDir, 'lift-rfile.out'), ...
+            'FileType', 'text', 'NumHeaderLines', 3);
+        skinf = readmatrix(fullfile(currentDir, 'skin_friction-rfile.out'), ...
+            'FileType', 'text', 'NumHeaderLines', 3);
+        clcdf = readmatrix(fullfile(currentDir, 'cl-cd-rfile.out'), ...
+            'FileType', 'text', 'NumHeaderLines', 3);
+        clf = readmatrix(fullfile(currentDir, 'lift_coefficient-rfile.out'), ...
+            'FileType', 'text', 'NumHeaderLines', 3);
+        cdf = readmatrix(fullfile(currentDir, 'drag_coefficient-rfile.out'), ...
+            'FileType', 'text', 'NumHeaderLines', 3);
+        
+        % Extract data
+        dragData = dragf(:, 1:2);
+        liftData = liftf(:, 1:2);
+        skinData = skinf(:, 1:2);
+        clcdData = clcdf(:, 1:2);
+        clData = clf(:, 1:2);
+        cdData = cdf(:, 1:2);
+        
+        % Time averaging (last numValues)
+        if size(dragData, 1) > numValues
+            dragData = dragData(end-numValues+1:end, :);
+        end
+        if size(liftData, 1) > numValues
+            liftData = liftData(end-numValues+1:end, :);
+        end
+        if size(skinData, 1) > numValues
+            skinData = skinData(end-numValues+1:end, :);
+        end
+        if size(clcdData, 1) > numValues
+            clcdData = clcdData(end-numValues+1:end, :);
+        end
+        if size(clData, 1) > numValues
+            clData = clData(end-numValues+1:end, :);
+        end
+        if size(cdData, 1) > numValues
+            cdData = cdData(end-numValues+1:end, :);
+        end
+        
+        % Calculate time averages
+        time_avgDragF = mean(dragData(:, 2));
+        time_avgLiftF = mean(liftData(:, 2));
+        time_avgSkinF = mean(skinData(:, 2));
+        time_avgClCdF = mean(clcdData(:, 2));
+        time_avgClF = mean(clData(:, 2));
+        time_avgCdF = mean(cdData(:, 2));
+        
+        % Calculate coefficients
+        Cd_calc(i) = (2 * time_avgDragF) / (rho * u^2 * A);
+        Cl_calc(i) = (2 * time_avgLiftF) / (rho * u^2 * A);
+        ClCd_calc(i) = Cl_calc(i) / Cd_calc(i);
+        Cl_solver(i) = time_avgClF;
+        Cd_solver(i) = time_avgCdF;
+        ClCd_solver(i) = time_avgClCdF;
+        Cf_solver(i) = time_avgSkinF;
+        
+        % Display results
+        fprintf('  Cl (calc): %.4f, Cl (CFD): %.4f\n', Cl_calc(i), Cl_solver(i));
+        fprintf('  Cd (calc): %.4f, Cd (CFD): %.4f\n', Cd_calc(i), Cd_solver(i));
+        fprintf('  Cl/Cd (calc): %.4f, Cl/Cd (CFD): %.4f\n', ClCd_calc(i), ClCd_solver(i));
+        
+    catch ME
+        warning('Error processing AoA = %.1f: %s', aoaValues(i), ME.message);
+        % Fill with NaN if processing fails
+        Cl_calc(i) = NaN;
+        Cd_calc(i) = NaN;
+        ClCd_calc(i) = NaN;
+        Cl_solver(i) = NaN;
+        Cd_solver(i) = NaN;
+        ClCd_solver(i) = NaN;
+    end
+end
+
+%% Generate AoA sweep plots
+figure('Position', [200 200 1200 900])
+
+% Cl vs AoA
+subplot(2,2,1)
+plot(aoaValues, Cl_calc, '-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(aoaValues, Cl_solver, '--s', 'LineWidth', 2, 'MarkerSize', 8)
+xlabel('Angle of Attack (degrees)')
+ylabel('Lift Coefficient (C_l)')
+title('Lift Coefficient vs AoA')
+legend('Calculated', 'CFD Solver', 'Location', 'best')
+grid on
+
+% Cd vs AoA
+subplot(2,2,2)
+plot(aoaValues, Cd_calc, '-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(aoaValues, Cd_solver, '--s', 'LineWidth', 2, 'MarkerSize', 8)
+xlabel('Angle of Attack (degrees)')
+ylabel('Drag Coefficient (C_d)')
+title('Drag Coefficient vs AoA')
+legend('Calculated', 'CFD Solver', 'Location', 'best')
+grid on
+
+% Cl/Cd vs AoA
+subplot(2,2,3)
+plot(aoaValues, ClCd_calc, '-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(aoaValues, ClCd_solver, '--s', 'LineWidth', 2, 'MarkerSize', 8)
+xlabel('Angle of Attack (degrees)')
+ylabel('Aerodynamic Efficiency (C_l/C_d)')
+title('C_l/C_d vs AoA')
+legend('Calculated', 'CFD Solver', 'Location', 'best')
+grid on
+
+% Cl vs Cd 
+subplot(2,2,4)
+plot(Cd_calc, Cl_calc, '-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Cd_solver, Cl_solver, '--s', 'LineWidth', 2, 'MarkerSize', 8)
+xlabel('Drag Coefficient (C_d)')
+ylabel('Lift Coefficient (C_l)')
+title('C_l vs C_d')
+legend('Calculated', 'CFD Solver', 'Location', 'best')
+grid on
+
+% Find optimal AoA
+[maxClCd, maxIdx] = max(ClCd_solver);
+fprintf('\nOptimal AoA: %.1f degrees with Cl/Cd = %.4f\n', aoaValues(maxIdx), maxClCd);
+
+%% Save results
+resultsTable = table(aoaValues', Cl_calc, Cd_calc, ClCd_calc, ...
+    Cl_solver, Cd_solver, ClCd_solver, ...
+    'VariableNames', {'AoA_deg', 'Cl_calc', 'Cd_calc', 'ClCd_calc', ...
+    'Cl_solver', 'Cd_solver', 'ClCd_solver'});
+writetable(resultsTable, 'AoA_sweep_results.csv'); %Rename me!
+fprintf('\nResults saved to AoA_sweep_results.csv\n');
+
+%% Plot Comparison of CFD data with previous research data
+figure('Position', [200 200 1200 900])
+
+% Cl vs AoA
+subplot(2,2,1)
+plot(aoaValues, Cl_solver, 'c-v', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Il_cl(:,1), Il_cl(:,2), 'k-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Kh_cl(:,1),Kh_cl(:,2) , 'm-*', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Li_cl(:,1),Li_cl(:,2) , 'b-^', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Ku_cl(:,1),Ku_cl(:,2) , 'r-s', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+xlabel('Angle of Attack (degrees)')
+ylabel('Lift Coefficient (C_l)')
+title('Lift Coefficient vs AoA, Ilio(2018)')
+legend('Matthews', 'Ilio', 'Khalid', 'Liu', 'Kurtulus', 'Location', 'best')
+grid on
+
+% CL solver signal trace
+subplot(2,2,2)
+plot(aoa4(:,3), aoa4(:,2), 'b-', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(aoa10(:,3), aoa10(:,2), 'm-', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(aoa16(:,3), aoa16(:,2), 'r-', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(aoa28(:,3), aoa28(:,2), 'g-', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+xlabel('Time Step Iteration')
+ylabel('Lift Coefficient (C_l)')
+title('Lift Coefficient (C_l) CFD Signal Trace')
+legend('AoA = 4', 'AoA = 10', 'AoA = 16', 'AoA = 28', 'Location', 'best')
+ylim ([0 2.5])
+grid on
+
+% Cd vs AoA
+subplot(2,2,3)
+plot(aoaValues, Cd_solver, 'c-v', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Il_cd(:,1), Il_cd(:,2), 'k-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Ku_cd(:,1),Ku_cd(:,2) , 'r-s', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+xlabel('Angle of Attack (degrees)')
+ylabel('Drag Coefficient (C_d)')
+title('Drag Coefficient vs AoA, Ilio(2018)')
+legend('Matthews', 'Ilio', 'Kurtulus', 'Location', 'best')
+grid on
+
+% % Cl/Cd vs AoA
+subplot(2,2,4)
+plot(aoaValues, ClCd_solver, 'c-v', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Il_clcd(:,1), Il_clcd(:,2), 'k-o', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+plot(Ku_clcd(:,1),Ku_clcd(:,2) , 'r-s', 'LineWidth', 2, 'MarkerSize', 8)
+hold on
+xlabel('Angle of Attack (degrees)')
+ylabel('Aerodynamic Efficiency (C_l/C_d)')
+title('C_l/C_d vs AoA, Ilio(2018)')
+legend('Matthews', 'Ilio', 'Kurtulus', 'Location', 'best')
+grid on
